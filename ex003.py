5aa318af75413af354b3f02464102fdf13f59fd0
Davidson Soares <103448810+DeisoTI@users.noreply.github.com>
2024-08-30 20:23:23 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,5 @@
+print('\033[1;30;42m=+=\033[m'*8, '\033[1;30;42mCalculadora de Soma\033[m', '\033[1;30;42m=+=\033[m'*8)
+x = int(input('\033[37mDigite um valor: \033[m'))
+y = int(input('\033[37mDigite outro valor: \033[m'))
+print('\033[1;32mA soma entre {} e {} é igual a: {}\033[m'.format(x, y, x+y))
+
