0cefc36a69a3493a0ae02d608dcf241471a075e7
Davidson Soares <103448810+DeisoTI@users.noreply.github.com>
2024-09-03 12:07:02 -0300

    ex007.py

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n = input('Digite o nome completo do aluno: ')
+x = float(input('Primeira nota do aluno: '))
+y = float(input('Segunda nota do aluno: '))
+m = (x+y)/2
+print('A média do aluno {} é: {}'.format(n, m))
